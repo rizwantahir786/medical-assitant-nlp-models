99871644cd2c33be10bf735a809af6946cbd4f89
Rizwan Tahir <rizwantahir730@gmail.com>
2023-09-15 05:04:26 +0100

    some fixes

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,20 @@
-# Use the official Python image as the base image
+# use the official Python image as the base image
 FROM python:3.8-slim
 
-# Set the working directory
+# set the working directory
 WORKDIR /app
 
-# Copy the requirements file into the container
+# copy the requirements file into the container
 COPY requirements.txt .
 
-# Install the dependencies
+# install the dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy the entire app into the container
+# copy the entire app into the container
 COPY . .
 
-# Expose port 5001
+# expose port 5001
 EXPOSE 5001
 
-# Command to run the application
+# command to run the application
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -5,17 +5,17 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# Load the BART model and tokenizer
+# load the BART model and tokenizer
 model = BartForConditionalGeneration.from_pretrained("./finetuned_bart_medical")
 tokenizer = BartTokenizer.from_pretrained("./finetuned_bart_medical")
 
 def generate_response(user_input):
-    # Check for greetings and return hardcoded responses
+    # check for greetings and return hardcoded responses
     greetings = ["hi", "hello", "hey", "greetings", "good day"]
     if user_input.lower() in greetings:
         return "Hello! How can I assist you?"
 
-    # If not a greeting, use the model to generate a response
+    # if not a greeting, use the model to generate a response
     input_tensor = tokenizer.encode(user_input, return_tensors="pt")
     output_tensor = model.generate(input_tensor, max_length=150, num_beams=5, early_stopping=True)
     response = tokenizer.decode(output_tensor[0], skip_special_tokens=True)
